2e18a5723d6dff080b1b8b318da97a2f87d656a2
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-04-01 18:20:13 +0300

    Update currency_copynew.py

--- currency_copynew.py
+++ currency_copynew.py
@@ -1,45 +1,47 @@
 import sqlite3
-import pb_copy
+import pb
 
 
 def inquiry(s):
-
-    conn = sqlite3.connect('currency.db')  # подключаемся к БД, если ее нет - создается
+    try:
+        conn = sqlite3.connect('E:\\currency.db')
+    except Exception as e:
+        print(e.__class__)
+        result = 'Нет связи с БД'
+        return result
     curs = conn.cursor()  # предназ для рабботы с запросами
-    curs.execute('''CREATE TABLE IF NOT EXISTS ccy (
+    conn.execute('''CREATE TABLE IF NOT EXISTS ccy (
                     id INTEGER NOT NULL PRIMARY KEY,
                     Abbreviation TEXT UNIQUE NOT NULL ,
                     Rate FLOAT NOT NULL,
                     last_updated timestamp default (strftime('%s', 'now')))'''  # в БД создаем таблицу с нужными столбцами и ограничениями
                  )
-
-
-
-    curs.execute('''SELECT Rate FROM ccy 
-            WHERE Abbreviation = {} and (strftime('%s') - last_updated <= 20)'''.format(s))  # запрос на вывод значения с таблицы, если значение в таблице                                                                                 # обновлялось более last_updated назад, значение не быдет выведено
+    curs.execute(f'''SELECT Rate FROM ccy 
+                        WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''')  # запрос на вывод значения с таблицы, если значение есть и оно не устаревшее                                                                                # обновлялось более last_updated назад, значение не быдет выведено
     row = curs.fetchall()  # в этой переменной сохраняем значение запроса
-    abbr, ofcrate, idd = pb_copy.get_exchange(s)
     if row:
         print('проверка(с базы)')
-        return row
-
-    curs.execute(f'''INSERT INTO ccy VALUES({idd}, {abbr}, {ofcrate}, strftime("%s"))  
-                    on conflict (id) do update set Rate=excluded.Rate, last_updated = excluded.last_updated''')  # вносим 3 строки значений, так же исключаем ограничение и обновляем записи в строке если вышло время обновления
-
-    conn.commit()
-    curs.execute('''SELECT Rate FROM ccy 
-                    WHERE Abbreviation = {} '''.format(s))  # делаем запрос в таблицу БД
-    row = curs.fetchall()  # сохраняем данные в переменной
-    if row:  # если нет в таблице значения - делаем действия ниже:
-        print('проверка(обновленное значение)')
-        return row
-    curs.execute('''SELECT Rate FROM ccy 
-                    WHERE Abbreviation = {} '''.format(s))
-    row2 = curs.fetchall()
-    if row2:
-        print('проверка(необновленное значение)')
-        return row2
-    result = 'Извините. Ошибка приложения'
+        return row[0][0]
+    try:
+        idd, abbr, ofcrate = pb.get_exchanges(s)
+        curs.execute(f'''INSERT INTO ccy VALUES({idd}, '{abbr}', {ofcrate}, strftime("%s"))  
+                                on conflict (id) do update set Rate=excluded.Rate, last_updated = excluded.last_updated''')
+        conn.commit()
+        curs.execute(f'''SELECT Rate FROM ccy 
+                            WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''')  # делаем запрос в таблицу БД
+        row = curs.fetchall()
+        if row:
+            print('проверка(обновленное значение)')
+            return row[0][0]
+    except Exception as e:
+        print(e.__class__)
+        curs.execute(f'''SELECT Rate FROM ccy 
+                            WHERE Abbreviation = '{s}' ''')
+        row = curs.fetchall()
+        if row:
+            print('проверка(необновленное значение)')
+            return row[0][0]
+    result = 'Ошибка приложения'
     return result
 
 
