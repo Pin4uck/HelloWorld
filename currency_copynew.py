6e7406580654b6828402c91a4646cff4eacc9d6a
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-04-05 22:22:04 +0300

    Убираем лишнее

--- currency_copynew.py
+++ currency_copynew.py
@@ -4,12 +4,10 @@ import pb
 
 def inquiry(s):
     try:
-        conn = sqlite3.connect('E:\\currency.db')
+        conn = sqlite3.connect('currency.db')
     except Exception as e:
-        print(e.__class__)
-        result = 'Нет связи с БД'
         return result
-    curs = conn.cursor()  # предназ для рабботы с запросами
+    curs = conn.cursor()
     conn.execute('''CREATE TABLE IF NOT EXISTS ccy (
                     id INTEGER NOT NULL PRIMARY KEY,
                     Abbreviation TEXT UNIQUE NOT NULL ,
@@ -19,7 +17,6 @@ def inquiry(s):
     curs.execute(f'''SELECT Rate FROM ccy 
                         WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''') 
     if row:
-        print('проверка(с базы)')
         return row[0][0]
     try:
         idd, abbr, ofcrate = pb.get_exchanges(s)
@@ -27,7 +24,6 @@ def inquiry(s):
                                 on conflict (id) do update set Rate=excluded.Rate, last_updated = excluded.last_updated''')
         conn.commit()
         if ofcrate:
-            print('проверка(обновленное значение)')
             return ofcrate
     except Exception as e:
         print(e.__class__)
@@ -35,10 +31,5 @@ def inquiry(s):
                             WHERE Abbreviation = '{s}' ''')
         row = curs.fetchall()
         if row:
-            print('проверка(необновленное значение)')
             return row[0][0]
-    result = 'Ошибка приложения'
-    return result
-
-
-inquiry(input())
+    return = 'Ошибка приложения'
