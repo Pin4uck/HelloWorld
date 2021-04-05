a9bffc6b4304285ac9f12f7e8aa45edf128698f4
Andrei <79600533+Pin4uck@users.noreply.github.com>
2021-04-05 22:15:39 +0300

    Убираем лишний SELECT

--- currency_copynew.py
+++ currency_copynew.py
@@ -14,11 +14,10 @@ def inquiry(s):
                     id INTEGER NOT NULL PRIMARY KEY,
                     Abbreviation TEXT UNIQUE NOT NULL ,
                     Rate FLOAT NOT NULL,
-                    last_updated timestamp default (strftime('%s', 'now')))'''  # в БД создаем таблицу с нужными столбцами и ограничениями
+                    last_updated timestamp default (strftime('%s', 'now')))''' 
                  )
     curs.execute(f'''SELECT Rate FROM ccy 
-                        WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''')  # запрос на вывод значения с таблицы, если значение есть и оно не устаревшее                                                                                # обновлялось более last_updated назад, значение не быдет выведено
-    row = curs.fetchall()  # в этой переменной сохраняем значение запроса
+                        WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''') 
     if row:
         print('проверка(с базы)')
         return row[0][0]
@@ -27,12 +26,9 @@ def inquiry(s):
         curs.execute(f'''INSERT INTO ccy VALUES({idd}, '{abbr}', {ofcrate}, strftime("%s"))  
                                 on conflict (id) do update set Rate=excluded.Rate, last_updated = excluded.last_updated''')
         conn.commit()
-        curs.execute(f'''SELECT Rate FROM ccy 
-                            WHERE Abbreviation = '{s}' and (strftime('%s') - last_updated <= 20)''')  # делаем запрос в таблицу БД
-        row = curs.fetchall()
-        if row:
+        if ofcrate:
             print('проверка(обновленное значение)')
-            return row[0][0]
+            return ofcrate
     except Exception as e:
         print(e.__class__)
         curs.execute(f'''SELECT Rate FROM ccy 
